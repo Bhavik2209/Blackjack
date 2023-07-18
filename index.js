dcec721e6bf1ce3e12f7a406cfb9a2869b2e7454
Bhavik2209 <117085679+Bhavik2209@users.noreply.github.com>
2023-07-18 13:16:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+let player={
+    name:"PLAYER",
+    chips:100
+    
+
+}
+
+let messageEl=document.getElementById("message-el")
+let firstcard =getRandomCard()
+let secondcard =getRandomCard()
+//  #array
+let cards=[]
+
+let hasblackjack=false
+let isalive=false
+message=""
+
+let sum = 0
+// console.log(sum)
+let sumEl=document.getElementById("sum-el")
+let cardsEl=document.getElementById("cards-el")
+let playerEl=document.getElementById("player-el")
+playerEl.textContent=player.name +": $" +player.chips
+
+
+
+
+function startgame(){
+    let firstcard =getRandomCard()
+    let secondcard =getRandomCard()
+    //  #array
+    cards=[firstcard,secondcard]
+    sum=firstcard+secondcard
+    isalive=true
+    rendergame()
+}
+function rendergame(){
+    sumEl.innerText= "sum : "+ sum
+    cardsEl.innerText="cards : "
+    for(let i=0;i<cards.length;i++)
+    {
+        cardsEl.innerText+=" "+ cards[i]
+    }
+    if (sum<=20){ 
+        message=("Do you want to draw a new card ? ")
+    }else if (sum === 21){
+        message=("Wohoo! you've got blackjack ! ")
+        hasblackjack=true
+    } else{
+        message=("You are out of the game ! ")
+        isalive=false
+    }
+    messageEl.innerText=message
+}
+
+function newcard(){
+    if(isalive===true && hasblackjack===false){
+        let card = getRandomCard()
+        sum+=card
+        cards.push(card)
+        rendergame()
+    }
+    
+}
+
+function getRandomCard(){
+    let x = Math.floor((Math.random() * 13) + 1);
+    if (x>10){
+        return 10
+    } else if(x===1){
+        return 11
+    } else{
+        return x
+    }
+}
